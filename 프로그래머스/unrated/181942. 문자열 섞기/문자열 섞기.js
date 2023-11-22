556676c4998b86d7176e3eeff3014cd64dcced11
Morgan Kim <jasinis102@gmail.com>
2023-11-22 10:47:29 +0900

    [level unrated] Title: 문자열 섞기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.js
@@ -1,11 +1,9 @@
 function solution(str1, str2) {
-    const arr1 = Array.from(str1);
-    const arr2 = Array.from(str2);
+    const [arr1, arr2] = [Array.from(str1), Array.from(str2)];
     const count = arr1.length;
     const answer = [];
     for (let i=0; i < count; i++) {
-    	answer.push(arr1.shift());
-    	answer.push(arr2.shift());
+    	answer.push(arr1.shift(), arr2.shift());
     }
     return answer.join('');
 }
